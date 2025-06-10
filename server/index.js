55e9f839a9ef80c88341b7dd7740d935f1453543
Moh123S <mohitsharma782000@gmail.com>
2025-06-10 17:20:35 +0530

    done

--- server/index.js
+++ server/index.js
@@ -4,15 +4,19 @@ const dotenv = require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-
+//use express.json() to get data into json format
 app.use(express.json());
-
+//Port 
 const PORT = process.env.PORT || 5500;
 
+//use cors
 app.use(cors());
 
+//import routes
 const TodoItemRoute = require('./routes/todoItems');
 
+
+//connect to mongodb ..
 mongoose.connect(process.env.DB_CONNECT)
 .then(()=> console.log("Database connected"))
 .catch(err => console.log(err))
@@ -22,4 +26,5 @@ app.use('/', TodoItemRoute);
 
 
 
-app.listen(PORT, ()=> console.log("Server connected"));
+//connect to server
+app.listen(PORT, ()=> console.log("Server connected") );
